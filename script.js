60cab352e6f92409c09bb7c6f9b8cf838aa53916
jood <84761277+rcins@users.noreply.github.com>
2024-06-26 23:03:11 -0500

    Update script2.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.createElement('button');
-    button.textContent = '';
-    button.addEventListener('click', function() {
-        alert('Hello, welcome to my personal website!');
-    });
-    document.body.appendChild(button);
-});
+
